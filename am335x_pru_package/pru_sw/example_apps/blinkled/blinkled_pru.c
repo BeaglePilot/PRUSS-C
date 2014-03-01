0278407c5ba5a4387c36d5535ccf6167de4cb793
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2014-03-01 19:41:32 +0530

    Resolve issue regarding halting of PRU application
    the assembly code for exiting the code is added using asm(const char* ASM_string). This is a temporary work around until any correct way of exiting is found using C code.

--- am335x_pru_package/pru_sw/example_apps/blinkled/blinkled_pru.c
+++ am335x_pru_package/pru_sw/example_apps/blinkled/blinkled_pru.c
@@ -4,7 +4,7 @@
 #define GPIO_CLEARDATAOUT 	0x190
 #define GPIO_SETDATAOUT 	0x194
 
-int main()
+void main()
 {
 	(*(volatile unsigned long *)(0x4804c194)) = 7<<22 ;
 	for(i=0;i<0x00a00000;i++);
@@ -14,5 +14,8 @@ int main()
 	for(i=0;i<0x00a00000;i++);
 	(*(volatile unsigned long *)(0x4804c190)) = 7<<22;
 	for(i=0;i<0x00a00000;i++);
-	return 0;
+	
+	/*Exiting procedure*/
+	asm(" LDI R3.b0, 35");			// Send notification to Host for program completion
+	asm(" HALT");
 }
