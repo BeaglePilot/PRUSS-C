65a76654cdd72ca27ea227776acbf9dc30862891
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2014-04-05 22:16:55 +0530

    Update example pru code as per new version of PRU compiler 2.0.0B1.

--- am335x_pru_package/pru_sw/example_apps/blinkled/blinkled_pru.c
+++ am335x_pru_package/pru_sw/example_apps/blinkled/blinkled_pru.c
@@ -3,14 +3,19 @@
 #define GPIO1 			(*(volatile unsigned long *)(0x4804c000))		// The address of the GPIO1 
 #define GPIO_CLEARDATAOUT 	0x190
 #define GPIO_SETDATAOUT 	0x194
+#define SYSCFG                  (*(&C4+0x01))
+int C4 __attribute__((cregister("MEM",near),peripheral));	//only compatible with v1.1.0B1 +
+								//add following lines to MEMORY{} in lnk.cmd
+								//PAGE 2:
+								//	MEM : o = 0x00026000 l = 0x00002000 CREGISTER=4
+
+volatile register unsigned int __R31;
 
 void main()
 {
 	/*Intialise OCP Master port for accessing external memories*/
-	asm(" LBCO &r0, C4, 4, 4");
-    	asm(" CLR r0, r0, 4");         // Clear SYSCFG[STANDBY_INIT] to enable OCP master port
-    	asm(" SBCO &r0, C4, 4, 4");
-	
+        SYSCFG&=0xFFFFFFEF;       // Clear SYSCFG[STANDBY_INIT] to enable OCP master port
+    	
 	/*Start Main Code*/
 	int i;
 	(*(volatile unsigned long *)(0x4804c194)) = 7<<22 ;
@@ -22,7 +27,8 @@ void main()
 	(*(volatile unsigned long *)(0x4804c190)) = 7<<22;
 	for(i=0;i<0x00a00000;i++);
 	
-	/*Exiting procedure*/
-	asm(" LDI R31.b0, 35");			// Send notification to Host for program completion
-	asm(" HALT");
+        /*Exiting procedure*/
+	__R31 = 35;			// Send notification to Host for program completion
+	__halt();			//only compatible with v2.0.0B1 + for lower verions of Compiler use
+					//asm(" HALT");
 }
